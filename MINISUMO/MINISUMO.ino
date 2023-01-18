0933355f735e66aeaa714a2102273d64feed3d8c
Toño <tonyshauder@gmail.com>
2023-01-18 16:08:46 -0600

    Primera versión del proyecto ya se puede controlar el motor derecho

--- MINISUMO/MINISUMO.ino
+++ MINISUMO/MINISUMO.ino
@@ -1,9 +1,22 @@
-void setup(){
-    int motorA=2;
-    int motorB=3;
-    pinMode(motorA,OUTPUT);
-    pinMode(motorB,OUTPUT);
-}
-void loop(){
+// Inicializamos el motor derecho
+int mA =11;
+int mA1=10;
+//Inicializamos el motor izquierdo
+int mB=9;
+int mB1=8;
 
+void setup() {
+  pinMode(mA, OUTPUT);
+  pinMode(mA1, OUTPUT);
+  pinMode(mB, OUTPUT);
+  pinMode(mB1, OUTPUT);
+}
+ 
+void loop() {
+  digitalWrite(mA, HIGH);
+  digitalWrite(mA1, LOW);
+  delay(3000);
+  digitalWrite(mA, LOW);
+  digitalWrite(mA1, HIGH);
+  delay(3000);
 }
